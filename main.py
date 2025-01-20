b09332be178fe8e3c0033bd83318b29d4fdf1f2e
sindiziu1 <sziu21@epoka.edu.al>
2025-01-20 14:04:41 +0100

    Initial .env and main file creation

---
+++ main.py
@@ -0,0 +1,17 @@
+import os
+from crate import client
+from dotenv import load_dotenv
+
+load_dotenv()
+
+cluster_connection = client.connect(
+    servers=os.getenv("CLUSTER_HOST"),
+    username=os.getenv("CLUSTER_USERNAME"),
+    password=os.getenv("CLUSTER_PASSWORD")
+)
+
+with cluster_connection:
+    cursor = cluster_connection.cursor()
+    cursor.execute("SELECT name FROM sys.cluster")
+    result = cursor.fetchone()
+    print(result)
