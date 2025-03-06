f0463ba292258e8ea434632f732e5bcad265a4e0
sindiziu1 <sziu21@epoka.edu.al>
2025-03-06 16:14:33 +0100

    Uncommented table creation

--- main.py
+++ main.py
@@ -11,12 +11,11 @@ load_dotenv()
 cratedb_client = create_cratedb_client()
 
 # Create infrastructure
-
-# create_database_objects(cratedb_client, "sql/wfigs.sql")
-# create_database_objects(cratedb_client, "sql/wfigs-geo.sql")
+create_database_objects(cratedb_client, "sql/wfigs.sql")
+create_database_objects(cratedb_client, "sql/wfigs-geo.sql")
 create_database_objects(cratedb_client, "sql/emergency-calls.sql")
 
 # Data ingestion
-# ingest_csv(cratedb_client, "data/wfigs-partial.csv", table_name="wfigs")
-# ingest_geojson(cratedb_client, "data/wfigs.geojson")
+ingest_csv(cratedb_client, "data/wfigs-partial.csv", table_name="wfigs")
+ingest_geojson(cratedb_client, "data/wfigs.geojson")
 ingest_csv(cratedb_client, "data/emergency_calls.csv", delimiter=";", table_name="emergency_calls")
